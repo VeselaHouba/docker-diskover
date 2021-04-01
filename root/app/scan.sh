45f71a0c0383044a8a38aa3acd861f00cfd20dbd
Michalek <michalek@m-cloud.cz>
2021-04-01 15:44:07 +0200

    Added option to define user for scan

--- root/app/scan.sh
+++ root/app/scan.sh
@@ -1,13 +1,19 @@
+#!/usr/bin/env bash
 #!/usr/bin/with-contenv bash
 set -e
 if [ $# -ne 2 ]; then
-  echo "Usage $0 hostname remote_path"
+  echo "Usage $0 [user@]hostname remote_path"
   exit 1
 fi
 HOST="${1}"
+if [[ "${HOST}" == *"@"* ]]; then
+  HOSTONLY="$(echo ${HOST}|awk -F'@' '{print $2}')"
+else
+  HOSTONLY="${HOST}"
+fi
 REMOTE_PATH="${2}"
 echo "Mounting sshfs"
-mkdir -p /scan/"${HOST}"
-sshfs -o ro,allow_other -o StrictHostKeyChecking=no "${HOST}:${REMOTE_PATH}" /scan/"${HOST}"
+mkdir -p /scan/"${HOSTONLY}"
+sshfs -o ro,allow_other -o StrictHostKeyChecking=no "${HOST}:${REMOTE_PATH}" /scan/"${HOSTONLY}"
 echo "Starting scan"
-/app/diskover/diskover.py -d /scan/"${HOST}" -i diskover-"${HOST}" -F
+/app/diskover/diskover.py -d /scan/"${HOSTONLY}" -i diskover-"${HOSTONLY}" -F
